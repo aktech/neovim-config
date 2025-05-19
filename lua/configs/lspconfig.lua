c46c9a84419fbf698f8321e70659da52c0e8a2af
Amit Kumar <dtu.amit@gmail.com>
2025-05-19 15:25:31 +0100

    add ts_ls server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright", "rust-analyzer" }
+local servers = { "html", "cssls", "pyright", "rust-analyzer", "ts_ls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
