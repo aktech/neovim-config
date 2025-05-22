63a06bd56232ef76495d70dc248d7c681354cab1
Amit Kumar <dtu.amit@gmail.com>
2025-05-22 19:23:20 +0100

    add lspsaga keybindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,10 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- Lspsaga keymap
--- Lspsaga keybindings
-map("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { desc = "Goto Definition" })
 map("n", "gp", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek Definition" })
-map("n", "K", "<cmd>Lspsaga hover_doc<CR>", { desc = "Hover Doc" })
-
+map("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { desc = "Peek Definition" })
+map("n", "gh", "<cmd>Lspsaga hover_doc<CR>", { desc = "Hover Doc" })
+map("n", "gr", "<cmd>Lspsaga finder<CR>", { desc = "Find references" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
