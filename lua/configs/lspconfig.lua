92d32f39735362baf2a0defead4472d6e2aa3238
Amit Kumar <dtu.amit@gmail.com>
2025-05-22 21:58:26 +0100

    add ruff for most

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,33 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright", "rust-analyzer", "ts_ls" }
+local servers = { "html", "cssls", "pyright", "rust-analyzer", "ts_ls", "ruff"}
+
+vim.lsp.config('ruff', {
+  init_options = {
+    settings = {
+      -- Ruff language server settings go here
+      args = {
+        '--ignore=I001'
+      },
+    }
+  }
+})
+
+vim.lsp.config('pyright', {
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
+})
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,7 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("n", "gp", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek Definition" })
-map("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { desc = "Peek Definition" })
+map("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { desc = "Go to Definition" })
 map("n", "gh", "<cmd>Lspsaga hover_doc<CR>", { desc = "Hover Doc" })
 map("n", "gr", "<cmd>Lspsaga finder<CR>", { desc = "Find references" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
