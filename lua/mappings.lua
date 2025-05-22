c2d75a51050c9fed4b4080e739055a8c3729b613
Amit Kumar <dtu.amit@gmail.com>
2025-05-22 17:49:37 +0100

    add lspsaga

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Lspsaga keymap
+-- Lspsaga keybindings
+map("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { desc = "Goto Definition" })
+map("n", "gp", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek Definition" })
+map("n", "K", "<cmd>Lspsaga hover_doc<CR>", { desc = "Hover Doc" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,6 +14,18 @@ return {
   },
 
   {
+    'nvimdev/lspsaga.nvim',
+    config = function()
+      require("lspsaga").setup({})
+    end,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    lazy = false,
+  },
+
+  {
     'rmagatti/auto-session',
     lazy = false,
 
