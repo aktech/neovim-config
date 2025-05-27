951008bec6917fb7e56ad9e9ad943895c6466240
Amit Kumar <dtu.amit@gmail.com>
2025-05-27 20:24:57 +0100

    make python diagnostic mode to workspace

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -27,6 +27,7 @@ vim.lsp.config('pyright', {
       analysis = {
         -- Ignore all files for analysis to exclusively use Ruff for linting
         ignore = { '*' },
+        diagnosticMode = "workspace",
       },
     },
   },
