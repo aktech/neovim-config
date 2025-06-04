39563b23c2eac436495490b10c150385e319b38a
Amit Kumar <dtu.amit@gmail.com>
2025-06-04 22:49:15 +0100

    change comments color

--- lua/plugins/configs/nvim-treesitter.lua
+++ lua/plugins/configs/nvim-treesitter.lua
@@ -1,18 +1,22 @@
-return   {
+return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter.configs").setup {
       ensure_installed = { "lua", "javascript", "python", "html", "css" },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<M-Up>",        -- Option + Up to start
-          node_incremental = "<M-Up>",      -- Option + Up to expand
-          node_decremental = "<M-Down>",    -- Option + Down to shrink
-          scope_incremental = "<Tab>",      -- Keep Tab for scope
+          init_selection = "<M-Up>", -- Option + Up to start
+          node_incremental = "<M-Up>", -- Option + Up to expand
+          node_decremental = "<M-Down>", -- Option + Down to shrink
+          scope_incremental = "<Tab>", -- Keep Tab for scope
         },
       },
       textobjects = {
@@ -27,6 +31,13 @@ return   {
           },
         },
       },
+    }
+
+    -- Highlighting
+    -- Define custom highlight groups for docstrings
+    vim.api.nvim_set_hl(0, "@string.documentation", {
+      fg = "#808080", -- Gray color for docstrings
+      italic = true, -- Make docstrings italic
     })
   end,
 }
