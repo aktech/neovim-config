9ad197b4399b2f8c0f5a134508c2f2315689fb03
Amit Kumar <dtu.amit@gmail.com>
2025-06-05 00:18:17 +0100

    add diagnostics colors

--- lua/options.lua
+++ lua/options.lua
@@ -6,7 +6,7 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 -- Configure diagnostics to be less overwhelming
-vim.diagnostic.config({
+vim.diagnostic.config {
   virtual_text = {
     severity = { min = vim.diagnostic.severity.WARN },
     spacing = 2,
@@ -34,10 +34,17 @@ vim.diagnostic.config({
   },
   severity_sort = true,
   update_in_insert = false,
-})
+}
 
 -- Define lighter diagnostic highlight groups
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#d80e52", bg = "NONE", italic = true })
+-- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#d80e52", bg = "NONE", italic = true })
+-- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#B2BEB5", bg = "NONE", italic = true })
+-- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#89b4fa", bg = "NONE", italic = true })
+-- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#a6e3a1", bg = "NONE", italic = true })
+
+-- darker shades of the same hues (~75 % darker than the fg)
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#d80e52", bg = "#3b071c", italic = true })
+-- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn",  { fg = "#B2BEB5", bg = "#2c2f2d", italic = true })
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#B2BEB5", bg = "NONE", italic = true })
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#89b4fa", bg = "NONE", italic = true })
-vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#a6e3a1", bg = "NONE", italic = true })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#89b4fa", bg = "#222d3e", italic = true })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#a6e3a1", bg = "#22372a", italic = true })
