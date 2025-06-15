a3a262d6bc010178db3281ca0c272f0ed30f0dba
Amit Kumar <dtu.amit@gmail.com>
2025-06-15 17:30:46 +0100

    customize statusline to show file relative path

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -25,4 +25,22 @@ M.base46 = {
 --      }
 --}
 
+M.ui = {
+  statusline = {
+    theme = "minimal", -- "default", "minimal", "vscode", "lsp", "gruvbox", "catppuccin"
+    separator_style = "default", -- "arrow", "block", "slant", "slant_double", "slant_thin", "slant_thick"
+    modules = {
+      -- to show full relative path of the file in the project
+      file = function()
+        local path = vim.fn.expand "%:."
+        if path == "" then
+          return "%#St_file_info# [No Name] "
+        end
+        local modified = vim.bo.modified and " +" or ""
+        return "%#St_file_info# " .. path .. modified .. " "
+      end,
+    },
+  },
+}
+
 return M
